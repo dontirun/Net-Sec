61a7924c25a7541f56ff1cf8aa5b9a7dedfc3b6c
= <jkearisman@wpi.edu>
2016-09-27 17:59:26 -0400

    Removed old comments

--- client/build_packet.h
+++ client/build_packet.h
@@ -6,13 +6,10 @@
 
 #define TCP_HEADER_BYTES 20
 
-//uint8_t* build_ip_header( uint32_t src, uint32_t dest, uint8_t ttl, uint16_t len );
-
-//uint8_t* build_tcp_header( char* data, uint32_t len );
-
 int send_packet( uint32_t src, uint32_t dest, uint8_t ttl_high, uint8_t ttl_low, char* data, uint16_t len_data );
 
 void print_header( uint8_t* header, size_t len );
+
 #endif
 
 
