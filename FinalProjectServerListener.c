04c095a09677b1f22ce8c55ef0dab4fcf0690edc
Arun Donti <dontirun@gmail.com>
2016-09-27 18:15:52 -0400

    Fixing things with libpcap

--- FinalProjectServerListener.c
+++ FinalProjectServerListener.c
@@ -1,6 +1,8 @@
 //Arun Donti Enhanced Client
 //This code has been adapted from :
-//TCP/IP Sockets in C Practical Guide for Programmers, Second Edition, Donahoo and Calvert
+//http://www.tcpdump.org/sniffex.c
+#define SIZE_ETHERNET 14
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,22 +14,39 @@
 #include <pcap.h>
 
 
+// IP header 
+struct sniff_ip {
+        u_char  ip_vhl;                 // version << 4 | header length >> 2
+        u_char  ip_tos;                 // type of service
+        u_short ip_len;                 // total length
+        u_short ip_id;                  // identification
+        u_short ip_off;                 // fragment offset field
+        #define IP_RF 0x8000            // reserved fragment flag 
+        #define IP_DF 0x4000            // dont fragment flag
+        #define IP_MF 0x2000            // more fragments flag
+        #define IP_OFFMASK 0x1fff       // mask for fragmenting bits
+        u_char  ip_ttl;                 // time to live
+        u_char  ip_p;                   // protocol
+        u_short ip_sum;                 // checksum
+        struct  in_addr ip_src,ip_dst;  // source and dest address
+};
 
 
 static const int MAXPENDING = 5; // Maximum outstanding connection requests
+
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
 
 int main(int argc, char *argv[]) {
 
-        pcap_t *handle;			/* Session handle */
-        char *dev;			/* The device to sniff on */
-        char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
-        struct bpf_program fp;		/* The compiled filter */
-        char filter_exp[] = "port 80";	/* The filter expression */
-        bpf_u_int32 mask;		/* Our netmask */
-        bpf_u_int32 net;		/* Our IP */
-        struct pcap_pkthdr header;	/* The header that pcap gives us */
-        const u_char *packet;		/* The actual packet */
+        pcap_t *handle;			// Session handle 
+        char *dev;			// The device to sniff on 
+        char errbuf[PCAP_ERRBUF_SIZE];	// Error string 
+        struct bpf_program fp;		//The compiled filter 
+       // char filter_exp[] = "";	// The filter expression
+        bpf_u_int32 mask;		// Our netmask
+        bpf_u_int32 net;		// Our IP
+        struct pcap_pkthdr header;	// The header that pcap gives us
+        const u_char *packet;		// The actual packet 
 
         dev = pcap_lookupdev(errbuf);
 
@@ -48,7 +67,8 @@ int main(int argc, char *argv[]) {
 			fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
 			return(2);
 		}
-		/* Compile and apply the filter */
+		/*
+		// Compile and apply the filter 
 		if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
 			fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
 			return(2);
@@ -57,10 +77,9 @@ int main(int argc, char *argv[]) {
 			fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
 			return(2);
 		}
-		/* Grab a packet until error occurs */
+		*/
+		// Grab a packet until error occurs //
 		packet = pcap_loop(handle,-1,got_packet,NULL);
-		/* Print its length */
-		printf("Jacked a packet with length of [%d]\n", header.len);
 		pcap_close(handle);
 
 	 }
@@ -69,25 +88,25 @@ int main(int argc, char *argv[]) {
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 	{
 
-		static int count = 1;                   /* packet counter */
+		static int count = 1;                   // packet counter 
 		
-		/* declare pointers to packet headers */
-		const struct sniff_ethernet *ethernet;  /* The ethernet header [1] */
-		const struct sniff_ip *ip;              /* The IP header */
-		const struct sniff_tcp *tcp;            /* The TCP header */
-		const char *payload;                    /* Packet payload */
+		// declare pointers to packet headers 
+	//	const struct sniff_ethernet *ethernet;  // The ethernet header [1] 
+		const struct sniff_ip *ip;              // The IP header 
+	//	const struct sniff_tcp *tcp;            // The TCP header 
+		const char *payload;                    // Packet payload 
 
 		int size_ip;
-		int size_tcp;
+	//	int size_tcp;
 		int size_payload;
 		
 		printf("\nPacket number %d:\n", count);
 		count++;
 		
-		/* define ethernet header */
-		ethernet = (struct sniff_ethernet*)(packet);
+		// define ethernet header 
+	//	ethernet = (struct sniff_ethernet*)(packet);
 		
-		/* define/compute ip header offset */
+		// define/compute ip header offset 
 		ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
 		size_ip = IP_HL(ip)*4;
 		if (size_ip < 20) {
@@ -95,71 +114,11 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
 			return;
 		}
 
-		/* print source and destination IP addresses */
+		// print source and destination IP addresses 
 		printf("       From: %s\n", inet_ntoa(ip->ip_src));
-		printf("         To: %s\n", inet_ntoa(ip->ip_dst));
-		
-		/* determine protocol */	
-		switch(ip->ip_p) {
-			case IPPROTO_TCP:
-				printf("   Protocol: TCP\n");
-				break;
-			case IPPROTO_UDP:
-				printf("   Protocol: UDP\n");
-				return;
-			case IPPROTO_ICMP:
-				printf("   Protocol: ICMP\n");
-				return;
-			case IPPROTO_IP:
-				printf("   Protocol: IP\n");
-				return;
-			default:
-				printf("   Protocol: unknown\n");
-				return;
-		}
-		
-		/*
-		*  OK, this packet is TCP.
-		*/
-		
-		/* define/compute tcp header offset */
-		tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
-		size_tcp = TH_OFF(tcp)*4;
-		if (size_tcp < 20) {
-			printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
-			return;
-		}
-		
-		printf("   Src port: %d\n", ntohs(tcp->th_sport));
-		printf("   Dst port: %d\n", ntohs(tcp->th_dport));
-		
-		/* define/compute tcp payload (segment) offset */
-		payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
-		
-		/* compute tcp payload (segment) size */
-		size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
-		
-		/*
-		* Print payload data; it might be binary, so don't just
-		* treat it as a string.
-		*/
-		if (size_payload > 0) {
-			printf("   Payload (%d bytes):\n", size_payload);
-			print_payload(payload, size_payload);
-		}
+		printf("         the ttl is: %s\n", inet_ntoa(ip->ip_ttl));
 
 	return;
 	}
-/*
-	void my_packet_handler(
-    u_char *args,
-    const struct pcap_pkthdr *packet_header,
-    const u_char *packet_body
-)
-{
-    print_packet_info(packet_body, *packet_header);
-    return;
-}
-*/
 
 }
