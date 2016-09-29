e702908f86e7a869a01f7c76a8a29839a10d91c8
Maurizio Vitale <mauriziovit@gmail.com>
2016-09-29 12:01:42 -0400

    This should be the skeleton for netfilter

---
+++ DNS/netfilter.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+#include <libnetfilter_queue/libnetfilter_queue.h>
+
+static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nfq_data *nfa, void *data) {
+	//This is the packet handler
+	//u_int32_t id = print_pkt(nfa);
+	printf("packet received\n");
+	return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
+}
+
+int main(int argc, char **argv)
+{
+	struct nfq_handle *h;
+	struct nfq_q_handle *qh;
+	struct nfnl_handle *nh;
+	int fd;
+	int rv;
+	char buf[4096];
+
+	/* Open library handle */
+	h = nfq_open();
+	if (!h) {
+		fprintf(stderr, "error during nfq_open()\n");
+		exit(1);
+	}
+
+	/* Unbind existing nf_queue handler */
+	if (nfq_unbind_pf(h, AF_INET) < 0) {
+		fprintf(stderr, "error during nfq_unbind_pf \n");
+		exit(1);
+	}
+
+	/* Bind nfnetlnk queue as handler for AF_INET */
+	if (nfq_bind_pf(h, AF_INET) < 0) {
+		fprintf(stderr, "error during nfq_bind_pf \n");
+		exit(1);
+	}
+
+	/* Bind socket to queue */
+	qh = nfq_create_queue(h, 0, &cb, NULL);
+	if (!qh) {
+		fprintf(stderr, "error during nfq_create_queue()\n");
+		exit(1);
+	}
+
+	fd = nfq_fd(h);
+	
+	while((rv = recv(fd, buf, sizeof(buf), 0)) && rv >= 0) {
+		printf("packet in queue\n");
+		nfq_handle_packet(h, buf, rv);
+	}
+
+	nfq_destroy_queue(qh);
+	nfq_close(h);
+	exit(0)
+}
