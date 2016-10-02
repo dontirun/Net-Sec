777113b36790f4c92a4dd00062e3cca424113bc8
Arun Donti <dontirun@gmail.com>
2016-10-02 13:02:56 -0400

    fixing some errors with creating command

--- FinalProjectServerListener.c
+++ FinalProjectServerListener.c
@@ -176,7 +176,9 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
 		// blacklist this ip , add SERVER NAT communication for blacklist
 		else{
 			printf("BLACKLIST %s\n",inet_ntoa(ip->ip_src));
-			char* banCommand ="BAN " + inet_ntoa(ip->ip_src);
+			char[20] banCommand; 
+			strcat(banCommand,"BAN ");
+			strcat(banCommand, inet_ntoa(ip->ip_src));
 			ssize_t banLength = strlen(banCommand);
 
 			// Send the string to the server
