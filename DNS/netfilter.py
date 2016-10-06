48bd86d162336ecea536709e4942f02817a86e57
Maurizio Vitale <mauriziovit@gmail.com>
2016-10-06 14:11:30 -0400

    More netfilter stuff

--- DNS/netfilter.py
+++ DNS/netfilter.py
@@ -1,9 +1,10 @@
 from netfilterqueue import NetfilterQueue
+from scapy.all import *
 from socket import *
 import sys
 
-s = NULL
-nfqueue = NULL
+s = None
+nfqueue = None
 
 # I asked Jesse if he trusted my code, he did, so it should work at some point
 
@@ -12,29 +13,48 @@ def exit():
         s.close()
     if nfqueue:
         nfqueue.unbind()
-
     sys.exit(0)
 
-def NAT_update(payload):
-    ip = payload
-    s.send('ADD:{}'.format(ip))
+def NAT_update(ip):
+    if s:
+        s.send('ADD;{}\0'.format(ip))
+	ipback = s.recv(20)
+    else:
+        print('ADD;{}\0'.format(ip))
 
-def handle_packet(pkt):
-    print pkt
-    print pkt.get_payload()
-    pkt.accept()
+def handle_dns(pkt):
+    print pkt[IP].dst
+    NAT_update(pkt[IP].dst)
+
+def handle_packet(packet):
+    data = packet.get_payload()
+    pkt = IP(data)
+    proto = pkt.proto
+    if proto is 0x11:
+        print "It's UDP"
+        if pkt[UDP].dport is 53:
+            print "It's DNS"
+            dns = handle_dns(pkt)
+            #packet.set_payload(dns)
+    #print pkt.get_payload()
+
+    packet.accept()
 
 def main(argv):
-    s = socket.socket(AF_INET, SOCK_STREAM)
+    s = socket(AF_INET, SOCK_STREAM)
 
     if len(argv) < 2:
         print('netfilter.py queuenum host port')
+	exit()
 
-    qnum = argv[0]
+    qnum = int(argv[0])
     host = argv[1]
-    port = argv[2]
+    port = int(argv[2])
 
     try:
+	print host
+	print port
+        s.settimeout(40)
         s.connect((host, port))
         print('NAT Server found')
     except Exception as e:
@@ -42,7 +62,7 @@ def main(argv):
         exit()
 
     nfqueue = NetfilterQueue()
-    nfqueue.bind(0, handle_packet)
+    nfqueue.bind(qnum, handle_packet)
     print("")
 
     try:
@@ -53,4 +73,4 @@ def main(argv):
     exit()
 
 if __name__ == "__main__":
-    main(argv[1:])
+    main(sys.argv[1:])
